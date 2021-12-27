da8c9653d7a5fa3f5f7a86c752b99a595fba4edf
TiroZit <nikus.master@gmail.com>
2021-12-27 15:35:06 +0500

    init

---
+++ gulp/config/path.js
@@ -0,0 +1,39 @@
+// Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./docs`;
+const srcFolder = `./src`;
+
+export const path = {
+  build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
+    fonts: `${buildFolder}/fonts/`,
+    files: `${buildFolder}/files/`
+  },
+  src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    sass: `${srcFolder}/sass/style.sass`,
+    html: `${srcFolder}/*.pug`,
+    files: `${srcFolder}/files/**/*.*`,
+    svgicons: `${srcFolder}/svg/*.svg`,
+  },
+  watch: {
+    js: `${srcFolder}/js/**/*.js`,
+    sass: `${srcFolder}/sass/**/*.sass`,
+    html: `${srcFolder}/**/*.pug`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
+    files: `${srcFolder}/files/**/*.*`,
+    //svgicons: `${srcFolder}/svg/**/*.svg`,
+  },
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: ``// Папка на удаленном сервере, куда залить
+}
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as Functions from './modules/functions.js';
+
+Functions.isWebp();
+
+// Сторонние плагины npm i -D Плагин
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper();
