5ee4cf99c2ff7495995df74c6d60afb247bf6f62
TiroZit <nikus.master@gmail.com>
2021-12-29 15:33:46 +0500

    js plugins

--- src/_js/app.js
+++ src/_js/app.js
@@ -2,10 +2,15 @@ import * as Functions from './modules/functions.js';
 
 Functions.isWebp();
 
-// Сторонние плагины npm i -D Плагин
+//Tabbis https://github.com/jenstornell/tabbis.js
+//import * as Tabs from './modules/tabbis.es6.js';
+//Tabs.Tabs();
 
+//import * as Modal from './modules/modal.js';
+//Modal.Modal();
+
+// SWIPER
 //import Swiper, { Navigation, Pagination } from 'swiper';
-
 //import 'swiper/css';
 //import 'swiper/css/navigation';
 //import 'swiper/css/pagination';
@@ -13,14 +18,33 @@ Functions.isWebp();
 //sliders.sliderMain();
 
 // LAZY-LOADING
-// npm install vanilla-lazyload
-
 //import lazyLoad from "vanilla-lazyload";
-
 //lazy-loading.lazy();
 
+// NOUISLIDER
+//import * as noUiSlider from 'nouislider';
+//import 'nouislider/dist/nouislider.css';
+
+// AOS - ANIMATE 
+//import AOS from 'aos';
+//import 'aos/dist/aos.css';
+//AOS.init();
+
+// SIMPLEBAR https://github.com/Grsmto/simplebar/tree/master/packages/simplebar
+//import 'simplebar';
+//import 'simplebar/dist/simplebar.css';
+
+// CHOICES https://github.com/Choices-js/Choices
+//import 'choices/public/assets/scripts/choices.min.js';
+//import 'choices/public/assets/styles/choices.min.css';
+
+// FANCYBOX https://fancyapps.com/docs/ui/fancybox
+//import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js";
+//import "fancyapps/ui/dist/fancybox.css";
+
+// Accordion https://github.com/michu2k/Accordion
+//import Accordion from 'accordion-js';
+//import 'accordion-js/dist/accordion.min.css';
 
 // FOCUS-VISIBLE
-// npm install --save focus-visible
-
 import focusVisible from "focus-visible";
